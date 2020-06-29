a918fa2c3735499aacae8baeec4e9af5d9b1c194
arifmh18 <firearif007@gmail.com>
2020-06-29 15:01:48 +0700

    Frame

---
+++ app/src/main/java/com/ardat/eventmanagement/Fragment/AgendaFragment.kt
@@ -0,0 +1,19 @@
+package com.ardat.eventmanagement.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ardat.eventmanagement.R
+
+class AgendaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_agenda, container, false)
+    }
+}
--- app/src/main/java/com/ardat/eventmanagement/MainActivity.kt
+++ app/src/main/java/com/ardat/eventmanagement/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.ardat.eventmanagement
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.ardat.eventmanagement.Fragment.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.home_menu -> {
+                val fragmentManager = supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                val fragment = EventFragment()
+                fragmentTransaction.replace(R.id.main_fragment, fragment)
+                fragmentTransaction.commit()
+            }
+            R.id.acara -> {
+                val fragmentManager = supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                val fragment = AgendaFragment()
+                fragmentTransaction.replace(R.id.main_fragment, fragment)
+                fragmentTransaction.commit()
+            }
+            R.id.team -> {
+                val fragmentManager = supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                val fragment = TeamFragment()
+                fragmentTransaction.replace(R.id.main_fragment, fragment)
+                fragmentTransaction.commit()
+            }
+            R.id.keuangan -> {
+                val fragmentManager = supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                val fragment = FinanceFragment()
+                fragmentTransaction.replace(R.id.main_fragment, fragment)
+                fragmentTransaction.commit()
+            }
+            R.id.peralatan -> {
+                val fragmentManager = supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                val fragment = EquipmentFragment()
+                fragmentTransaction.replace(R.id.main_fragment, fragment)
+                fragmentTransaction.commit()
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = EventFragment()
+        fragmentTransaction.replace(R.id.main_fragment, fragment)
+        fragmentTransaction.commit()
+
     }
 }
