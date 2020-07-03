d43c8e077560f77c7b17d400d557d31b078b26f1
ardianjafar <hendra0maulidan@gmail.com>
2020-07-03 22:31:14 +0700

    menambahkan activity CreatePeralatan & desain fragment Equipment

---
+++ app/src/main/java/com/ardat/eventmanagement/itemPeralatan/CreatePeralatan.kt
@@ -0,0 +1,13 @@
+package com.ardat.eventmanagement.itemPeralatan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ardat.eventmanagement.R
+
+class CreatePeralatan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_peralatan)
+
+    }
+}
