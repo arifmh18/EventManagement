525e2bed3aa197f4a2d89559169a3c61bd91bbb3
Andi <andi502.as@gmail.com>
2020-07-04 09:30:32 +0700

    [+]delete event db

--- app/src/main/java/com/ardat/eventmanagement/adapter/EventAdapter.kt
+++ app/src/main/java/com/ardat/eventmanagement/adapter/EventAdapter.kt
@@ -1,12 +1,16 @@
 package com.ardat.eventmanagement.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.ardat.eventmanagement.R
 import com.ardat.eventmanagement.model.Event
+import com.ardat.eventmanagement.utils.showToast
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.layout_item_event.*
 
@@ -37,6 +41,32 @@ class EventAdapter (private val context: Context) : RecyclerView.Adapter<EventAd
             tvEventNameItemEvent.text = item.eventName
             tvEventInfoEventItem.text = item.eventInfo
             tvEventDateEventItem.text = item.eventDate
+
+            val database = FirebaseDatabase.getInstance()
+            val databaseReference = database.getReference("event")
+
+            ivMenuItemEvent.setOnClickListener {
+                val popupMenu = PopupMenu(containerView.context, ivMenuItemEvent)
+                popupMenu.inflate(R.menu.menu_item_event)
+                popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener {
+                    return@OnMenuItemClickListener when (it.itemId){
+                        R.id.menuEventUpdate -> {
+                            Log.d("event item", "delete with uid : "+item.eventUid)
+                            true
+                        }
+                        R.id.menuEventDelete -> {
+                            databaseReference.child(item.eventUid).removeValue()
+                                .addOnSuccessListener {
+                                    showToast(containerView.context, "Berhasil hapus event")
+
+                                }
+                            true
+                        }
+                        else -> false
+                    }
+                })
+                popupMenu.show()
+            }
         }
     }
 
